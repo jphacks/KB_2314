582d026f65fe2a0feb5f48e1b3e0906ea980fe0c
k-tomo <tomo577@icloud.com>
2023-10-29 13:23:08 +0900

    traiiningの実装

---
+++ health_sleep/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/kadoyahirotomo/fvm/versions/3.13.6"
+export "FLUTTER_APPLICATION_PATH=/Users/kadoyahirotomo/Documents/GitHub/KB_2314/health_sleep"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/kadoyahirotomo/Documents/GitHub/KB_2314/health_sleep/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/kadoyahirotomo/Documents/GitHub/KB_2314/health_sleep/.dart_tool/package_config.json"
---
+++ health_sleep/ios/Pods/Target Support Files/sensors_plus/sensors_plus-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_sensors_plus : NSObject
+@end
+@implementation PodsDummy_sensors_plus
+@end
