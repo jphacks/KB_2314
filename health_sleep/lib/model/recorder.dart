53a84402e488dd97b6316cae86a567b1d51f8385
k-tomo <tomo577@icloud.com>
2023-10-31 08:15:42 +0900

    recorderモデルの修正

--- health_sleep/lib/model/recorder.dart
+++ health_sleep/lib/model/recorder.dart
@@ -22,37 +22,37 @@ class Recorder {
   List accel = [0.0, 0.0, 0.0];
   List velocity_record = [[0.0, 0.0, 0.0]];
   List trajectory = [[0.0, 0.0, 0.0]];
-
+/*
   double _calc_distance(List pos1, List pos2) {
     var distance =
     pow(pow(pos1[0] - pos2[0], 2) + pow(pos1[1] - pos2[1], 2) +
         pow(pos1[2] - pos2[2], 2), 0.5);
     return (distance * 100).floor() / 100;
   }
-
+*/
   Future<void> update(isInit) async {
-    /*
     if(isInit == true) {
       this.velocity = [0.0, 0.0, 0.0];
       this.trajectory.last = [0.0, 0.0, 0.0];
       this.velocity_record.last = [0.0, 0.0, 0.0];
-    }*/
+    }
 
     var accel_data = await userAccelerometerEvents.elementAt(0);
     this.accel = [
-      (accel_data.x * 10).round() / 10,
-      (accel_data.y * 10).round() / 10,
-      (accel_data.z * 10).round() / 10
+      accel_data.x.round(),
+      accel_data.y.round(),
+      accel_data.z.round()
     ];
 
     // 経過時間の計算
     this.time += dt;
     // 速度の計算
     this.velocity = [
-      (this.velocity[0] + accel[0] * dt * 10).round() / 10,
-      (this.velocity[1] + accel[1] * dt * 10).round() / 10,
-      (this.velocity[2] + accel[2] * dt * 10).round() / 10
+      (this.velocity_record.last[this.velocity_record.length-1] + accel[0] * dt) / 10,
+      (this.velocity_record.last[this.velocity_record.length-1] + accel[1] * dt) / 10,
+      (this.velocity_record.last[this.velocity_record.length-1] + accel[2] * dt) / 10
     ];
+    this.velocity_record.add(this.velocity);
 
     // 位置の計算
     var pos = [
